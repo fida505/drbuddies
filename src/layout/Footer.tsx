625a44305d408d5269581370ca1ef005d2482c77
FIDA FATHIMA N N <fidafathimannfida@gmail.com>
2026-06-29 14:29:50 +0530

    style: remove Lorem Ipsum dummy text from footer head section

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -30,7 +30,6 @@ function Footer() {
                             <div className="row g-3 align-items-center">
                                 <div className="col-xl-3 col-md-12 col-sm-6 wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="0.8s">
                                     <h3 className="title">Get in Touch with us</h3>
-                                    <p className="text">Lorem Ipsum is simply dummy</p>
                                 </div>
                                 {footerdata1.map((data, i) => (
                                     <div className="col-xl-3 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay={data.delay} data-wow-duration="0.8s" key={i}>
